0a309427484cace8f1128b6ca0e876759ebd86db
Gaston Bengolea Monzon <gastonbengolea@gmail.com>
2016-09-02 18:04:56 -0300

    Refactor Evaluator tests using Parser

--- evaluator_test.py
+++ evaluator_test.py
@@ -1,45 +1,35 @@
 from primitives import *
 from evaluator import Evaluator
-from parser import quoted
+from parser import Parser
 import unittest
 
 
 class EvaluatorTest(unittest.TestCase):
 
-    def test_00_nil_evaluates_to_nil(self):
+    def assertEvaluating(self, expr1, equals):
+        p = Parser()
         ev = Evaluator()
-        self.assertEquals(ev.evaluate(Nil()), Nil())
+        self.assertEquals(ev.evaluate(p.parse(expr1)), ev.evaluate(p.parse(equals)))
+
+    def test_00_nil_evaluates_to_nil(self):
+        self.assertEvaluating("nil", equals="nil")
 
     def test_01_number_evaluates_to_itself(self):
-        ev = Evaluator()
-        self.assertEquals(ev.evaluate(Number(4)), Number(4))
+        self.assertEvaluating("4", equals="4")
 
     def test_02_quoted_symbol_evaluates_to_quotation(self):
-        ev = Evaluator()
-        expr = List([Symbol("a"), Number(1)])
-        self.assertEqual(ev.evaluate(quoted(expr)), expr)
+        self.assertEvaluating("'(a 1)", equals="(a 1)")
 
     def test_03_primitive_function_plus_evaluates_the_sum(self):
-        ev = Evaluator()
-        expr = List([Symbol("+"), Number(3)])
-        self.assertEqual(ev.evaluate(expr), Number(3))
-
-        expr = List([Symbol("+"), Number(3), Number(5)])
-        self.assertEqual(ev.evaluate(expr), Number(8))
-
-        expr = List([Symbol("+"), Number(3), Number(5), Number(-1)])
-        self.assertEqual(ev.evaluate(expr), Number(7))
+        self.assertEvaluating("(+ 3)", equals="3")
+        self.assertEvaluating("(+ 3 5)", equals="8")
+        self.assertEvaluating("(+ 3 5 -1)", equals="7")
 
     def test_04_primitive_function_minus_evaluates_the_difference(self):
-        ev = Evaluator()
-        expr = List([Symbol("-"), Number(3), Number(5)])
-        self.assertEqual(ev.evaluate(expr), Number(-2))
+        self.assertEvaluating("(- 3 5)", equals="-2")
 
     def test_05_other_primitive_arithmetic_functions(self):
-        ev = Evaluator()
-        expr = List([Symbol("*"), Number(3), Number(5)])
-        self.assertEqual(ev.evaluate(expr), Number(15))
+        self.assertEvaluating("(* 3 5)", equals="15")
+        self.assertEvaluating("(/ 10 2)", equals="5")
 
-        expr = List([Symbol("/"), Number(10), Number(2)])
-        self.assertEqual(ev.evaluate(expr), Number(5))
 
